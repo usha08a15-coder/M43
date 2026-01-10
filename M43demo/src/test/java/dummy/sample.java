f936a43cd3d2e9d6e16cde940506e76d4ea16ef5
kkaus <kkaus@192.168.1.4>
2026-01-10 08:16:47 +0530

    commit

---
+++ M43demo/src/test/java/dummy/sample.java
@@ -0,0 +1,15 @@
+package dummy;
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+
+	
+	}
+
+}
