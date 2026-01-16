921867a578f00ec9ce79ac63fa01289c5faa9d2a
kkaus <kkaus@Kaushal>
2026-01-16 08:39:06 +0530

    New changes

--- M43demo/src/test/java/dummy/Junior1.java
+++ M43demo/src/test/java/dummy/Junior1.java
@@ -4,7 +4,7 @@ public class Junior1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Junior 1 is trying to add new program");
+		System.out.println("Junior 1 is trying to add new program");
 	}
 
 }
